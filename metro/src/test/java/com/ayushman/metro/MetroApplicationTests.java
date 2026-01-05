75c8ea7fe6c08a431ce56106074b8e5219bd10e2
Ayushman1109 <manoharayushman@gmail.com>
2026-01-05 13:18:53 +0530

    initial commit

---
+++ metro/src/test/java/com/ayushman/metro/MetroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayushman.metro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
